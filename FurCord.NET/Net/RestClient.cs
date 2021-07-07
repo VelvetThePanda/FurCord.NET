cce98d6c9845373e6d67b36b6bd88cc6d1d63c19
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-07-07 16:25:49 -0400

    a

---
+++ FurCord.NET/Net/RestClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace FurCord.NET
+{
+	public sealed class RestClient
+	{
+		private readonly HttpClient _client;
+		private readonly HttpHeaders _headers;
+		
+		public RestClient(Uri baseUri, string userAgent)
+		{
+			var handler = new HttpClientHandler()
+			{
+				UseCookies = true,
+				AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+			};
+
+			_client = new(handler);
+			_client.BaseAddress = baseUri;
+			_client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", userAgent);
+		}
+	}
+}
---
+++ FurCord.NET/Net/RestResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+
+namespace FurCord.NET
+{
+	public class RestResponse
+	{
+		public int ResponseCode { get; private set; }
+		public string Content { get; private set; }
+
+		public RestResponse(HttpResponseMessage httpResponseMessage, string content)
+		{
+			ResponseCode = (int)httpResponseMessage.StatusCode;
+			Content = content;
+		}
+	}
+}
