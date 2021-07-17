895daccabb21fad7bec773ed6d5c7f8731a31c4c
Velvet <42438262+VelvetThePanda@users.noreply.github.com>
2021-07-17 05:41:46 -0400

    Add serilog to test project

--- tests/FurCord.NET.Testing/Program.cs
+++ tests/FurCord.NET.Testing/Program.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using FurCord.NET.Net;
 using FurCord.NET.Net.Enums;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace FurCord.NET.Testing
 {
@@ -11,12 +12,19 @@ namespace FurCord.NET.Testing
 		public static async Task Main(string[] args)
 		{
 			var container = new ServiceCollection();
+
+			var logConfig = new LoggerConfiguration()
+				.MinimumLevel.Verbose()
+				.WriteTo.Console(outputTemplate: "[{Timestamp:h:mm:ss ff tt}] [{Level:u3}] [{SourceContext}] {Message:lj} {Exception:j}{NewLine}");
+			
+			container.AddLogging(l => l.AddSerilog(logConfig.CreateLogger()));
+			
 			container.AddFurCordClient(new(File.ReadAllText("./token.txt")) {Intents = GatewayIntents.All});
 			var client = container.BuildServiceProvider().GetRequiredService<IDiscordClient>();
 			
 			await client.ConnectAsync();
 
-			await Task.Delay(8000);
+			await Task.Delay(20000);
 			
 			await Task.Delay(-1);
 		}
